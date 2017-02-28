786490b268188d478c20400de0a5fd80f86813e0
Abel Gaxiola <abel.gaxiola@pima.gov>
2017-02-28 15:51:29 -0700

    Replaced HTML tags with ngFor section

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { BlogService } from './blog.service';
+import { Component, OnInit } from '@angular/core';
+
+import { Blog } from './../blog/blog';
+
 
 @Component({
-    templateUrl: 'app/blog/blog.component.html'
+  templateUrl: 'app/blog/blog.component.html'
 })
 
-export class BlogComponent {
-    public pageTitle: string = 'Blog';
+export class BlogComponent implements OnInit {
+  pageTitle = 'Blog';
+  errorMessage: string;
+  blogs: Blog[];
+
+  constructor(private blogService: BlogService) { }
+
+  ngOnInit(): void {
+    this.blogService.getBlogs()
+      .subscribe(blogs => this.blogs = blogs,
+      error => this.errorMessage = <any>error);
+  }
+
 }
