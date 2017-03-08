892afce007e9a8da9a6636ef0302d1608e177450
Abel Gaxiola <abelgaxiola@outlook.com>
2017-03-07 20:52:30 -0700

    Update 4 posts and changed font-size

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -7,35 +7,35 @@ import { Blog } from './../blog/blog';
 import { BlogService } from './../blog/blog.service';
 
 @Component({
-    templateUrl: 'app/blog-detail/blog-detail.component.html'
+  templateUrl: 'app/blog-detail/blog-detail.component.html'
 })
 
 export class BlogDetailComponent implements OnInit {
-    blog: Blog;
-    errorMessage: string;
-    private sub: Subscription;
+  blog: Blog;
+  errorMessage: string;
+  private sub: Subscription;
 
-    constructor(
-        private route: ActivatedRoute,
-        private router: Router,
-        private blogService: BlogService
-    ) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private blogService: BlogService
+  ) { }
 
-    ngOnInit(): void {
-        this.sub = this.route.params.subscribe(
-            params => {
-                let id = +params['id'];
-                this.getBlog(id);
-            });
-    }
+  ngOnInit(): void {
+    this.sub = this.route.params.subscribe(
+      params => {
+        let id = +params['id'];
+        this.getBlog(id);
+      });
+  }
 
-    getBlog(id: number) {
-        this.blogService.getBlog(id).subscribe(
-            blog => this.blog = blog,
-            error => this.errorMessage = <any>error);
-    }
+  getBlog(id: number) {
+    this.blogService.getBlog(id).subscribe(
+      blog => this.blog = blog,
+      error => this.errorMessage = <any>error);
+  }
 
-    onBack(): void {
-        this.router.navigate(['/blog']);
-    }
+  onBack(): void {
+    this.router.navigate(['/blog']);
+  }
 }
